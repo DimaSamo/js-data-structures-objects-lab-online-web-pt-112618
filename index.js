e4265df1bec663448ea508ade55633f8ffea8d6f
Dmitri Samotessov <dimasamo96@gmail.com>
2019-07-10 21:03:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function deleteFromDriverByKey(driver, key){
   delete newArray[key]
   return newArray
 }
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+  delete obj[key];
+
+  return obj;
+}
